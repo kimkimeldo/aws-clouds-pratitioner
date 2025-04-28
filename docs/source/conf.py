9cc4aba0c1feaa854279b7915f8c686589a031cb
kimkimeldo <kimverliemaghari@gmail.com>
2025-04-28 15:59:30 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CC 6L'
+copyright = '2025, BSIT3'
+author = 'Eldo kimverlie'
 
 release = '0.1'
 version = '0.1.0'
